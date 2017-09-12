74da129e71b4d8d4ef34586526b4bd5b036bdfca
Miguel <migjaques@gmail.com>
2017-09-12 22:01:01 +0100

    Separate register for pokemon because of frame skipping

--- gym_rle/__init__.py
+++ gym_rle/__init__.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 for game in ['aladdin', 'bust_a_move', 'classic_kong', 'final_fight', 'f_zero', 'gradius_iii',
              'mortal_kombat', 'nba_give_n_go', 'super_mario_all_stars',
-             'super_mario_world', 'street_fighter_ii', 'tetris_and_dr_mario', 'wolfenstein', 'pokemon']:
+             'super_mario_world', 'street_fighter_ii', 'tetris_and_dr_mario', 'wolfenstein']:
         for obs_type in ['image', 'ram']:
             name = ''.join([g.capitalize() for g in game.split('_')])
             if obs_type == 'ram':
@@ -20,3 +20,20 @@ for game in ['aladdin', 'bust_a_move', 'classic_kong', 'final_fight', 'f_zero', 
                 tags={'wrapper_config.TimeLimit.max_episode_steps': 10000},
                 nondeterministic=nondeterministic,
             )
+
+for game in ['pokemon',]:
+    for obs_type in ['image', 'ram']:
+        name = ''.join([g.capitalize() for g in game.split('_')])
+        if obs_type == 'ram':
+            name = '{}-ram'.format(name)
+        nondeterministic = False
+
+        register(
+            id='{}-v0'.format(name),
+            entry_point='gym_rle.envs:RleEnv',
+            kwargs={'game': game, 'obs_type': obs_type, 
+                    'frameskip': 5, 'repeat_action_probability': 0.0},
+            tags={'wrapper_config.TimeLimit.max_episode_steps': 10000},
+            nondeterministic=nondeterministic,
+        )
+
